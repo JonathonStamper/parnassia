55f659d9f43e4436b087239a51a8015de0e10b85
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2024-10-24 13:51:41 +0200

    Dashboard navbar

---
+++ components/Navlink.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function Navlink({children, linkName, link}) {
+  const router = useRouter()
+
+  console.log(router.asPath)
+    // const [isHovered, setIsHovered] = useState()
+
+  return (
+  
+          <Link href={`/${link}`} className={`flex w-full gap-10 items-center text-x p-3 rounded-xl px-5 ${router.asPath === `/${link}` ? 'bg-[#3766C3]' : ''}`}>
+                <div  className='w-[36px]'>{children}</div>
+                {linkName}
+        </Link>  
+
+  )
+}
---
+++ pages/personeel.js
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function personeel() {
+
+    
+  return (
+<Navbar/>
+  )
+}
